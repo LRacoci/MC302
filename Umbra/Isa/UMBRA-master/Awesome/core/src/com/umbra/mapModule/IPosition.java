666a08a5bb3e50a35275381f4bb209881312c369
Lucas <racoci.0@gmail.com>
2015-05-27 21:38:02 -0300

    Mudando

--- Umbra/Isa/UMBRA-master/Awesome/core/src/com/umbra/mapModule/IPosition.java
+++
@@ -1,6 +0,0 @@
-package com.umbra.mapModule;
-/**
- * Created by Lucas on 26/04/2015.
- */
-public interface IPosition {
-}
--- Umbra/Isa/UMBRA-master/Awesome/core/src/com/umbra/mobModule/mobComponent/IPlayer.java
+++
@@ -1,8 +0,0 @@
-package com.umbra.mobModule.mobComponent;
-
-import mobModule.itemComponent.IInventory;
-
-public interface IPlayer extends IMob {
-	public void createInventory(int size);
-    public IInventory getInventory();
-}
